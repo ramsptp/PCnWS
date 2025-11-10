2e95aae93258174874a57b614c30cf6030d72766
Draiger <abhijithprasad1999@gmail.com>
2025-11-10 20:30:01 +0530

    supabase config

---
+++ src/main/java/com/sk/PCnWS/PCnWsApplication.java
@@ -0,0 +1,13 @@
+package com.sk.PCnWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PCnWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PCnWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sk/PCnWS/PCnWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.PCnWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PCnWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
